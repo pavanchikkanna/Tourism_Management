136ba34c4765945025bb4fe8c8dbb31a3602b1f5
pavanchikkanna <pavanravikumar1998@gmail.com>
2019-03-02 09:05:14 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,303 @@
 console.log("om nama shivaya");
-var express = require("express");
-var app = express();
-var port = 3000;
- 
-app.get("/", (req, res) => {
- res.send("Hello World");
+let http=require('http');
+let express=require('express');
+let mongoose=require('mongoose');
+let bodyParser=require('body-parser');
+let app=express();
+let bp=bodyParser.json();
+var nodemailer = require('nodemailer');
+
+mongoose.connect('mongodb://pavan:chikka1998@ds123465.mlab.com:23465/karunadu');
+//package document 
+let packageschema=new mongoose.Schema(
+    {
+    "name":String,
+    "type": String,
+    "cost": Number,
+    "duration": String,
+    "depaturedate":Date,
+    "depatureaddress":String,
+    "icon":String,
+    "slides": [String],
+    "itinerary": [String],
+    "description":String,
+    "visitingplaces": [String]
+    }
+);
+//booked package document ,which is the subdocument of user document
+let bookingschema=new mongoose.Schema(
+    {
+       
+                "packageId":String,
+                "packagename":String,
+                "packageicon":String,
+                "depaturedate":Date,
+                "enddate":Date,
+                "depatureaddress":String,
+                "duration":String,
+                "dobooking":Date,
+                "adults":Number,
+                "children":Number,
+                "totalcost":Number,
+                "payment":{"cardno":Number,"cardname":String,"dateofexpire":Date,"cvc":String}
+        
+    }
+);
+//user document
+let userschema=new mongoose.Schema(
+    {  
+        "accountname":String,
+        "email":String,
+        "password":String,
+        "phone":Number,
+        "dob":Date,
+        "address":{"street":String, "city":String, "district":String },
+        "bookedpackages":[bookingschema]
+            
+        
+
+    }
+);
+userschema.index({"email":1},{unique:true});//making the email field as index in schema level
+
+
+let packagesModel=new mongoose.model("packages",packageschema);
+
+let usersModel=new mongoose.model("users",userschema);
+let bookingmodel=new mongoose.model("booking",bookingschema);
+
+console.log("server running @ 3000");
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+app.listen(3000);
+
+//nodemailer
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'pavanravikumar1998@gmail.com',
+      pass: '9902587276'
+    }
+  });
+//this routing is used for sigin purpose
+app.post("/signin",bp,function(req,res)
+{
+   let sigiinMessage="";
+    console.log("signin called");
+    console.log(req.body);
+   usersModel.find({"email":req.body.email},'password',function(err,data)
+   {
+       if(data.length!=0)
+       {console.log(data[0])
+        if(data[0].password==req.body.password)
+        {
+            sigiinMessage=req.body.email;
+        }
+        else
+        {
+        sigiinMessage="invalidPassword"
+       }
+    }
+       else
+       sigiinMessage="invalidemail";
+       res.send({"message":sigiinMessage})
+   });
+   ;
 });
- 
-app.listen(port, () => {
- console.log("Server listening on port " + port);
+
+//this routing is used for retrieving all packages present in database
+//and sorting the query based on the depaturedate in ascending order
+app.get("/packages",function(req,res){
+    packagesModel.find({},null,{sort: 'depaturedate'},function(err,data){
+        
+        res.json(data);
+    });   
+});
+
+//this routing is used for querying the package documents present in packages collection based on the type field
+app.post("/fetchBySearch",bp,function(req,res)
+{
+    console.log(req.body.searchdata);
+    packagesModel.find({"type":req.body.searchdata},function(err,data)
+    {
+        console.log("search request")
+        res.json(data);
+    })
+})
+
+
+
+//this routing is used for querying the package document based on the _id field
+app.post("/fetchById",bp,function(req,res){
+    console.log("fetch by id called");
+    console.log(req.body.packageId);
+    packagesModel.find({"_id":req.body.packageId},function(err,data){
+        
+        res.json(data);
+    });   
+});
+
+////this routing is not used
+app.get("/logout",function(req,res)
+{   
+    
+  
+   console.log("detroyed successfully");
+
+});
+
+//this routing is used for signup
+app.post("/newuser",bp,function(req,res)
+{ var signupmssg="invalid"
+  usersModel.find({"email":req.body.email},'password',function(err,data)
+  {
+      if(data.length==0)
+      {var mailOptions = {
+        from: 'pavanravikumar1998@gmail.com',
+        to: req.body.email,
+        subject: 'karunadu tourism',
+        text: 'Thank you for registering with karunadu!'
+      // html:
+       //'<p><b>Thank you </b> for regitration</p>' 
+       
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            signupmssg="InvalidEmail"
+            res.send({"message":signupmssg})
+        } else {
+            usersModel(req.body).save(function(err,data)
+        {
+            console.log("added user");
+            signupmssg="valid"
+            res.send({"message":signupmssg})
+        });
+          console.log('Email sent: ' + info.response);
+        }
+      });
+        
+      }
+      else
+      res.send({"message":signupmssg})
+  })
+});
+
+//this routing is used for querying profile details of the user
+app.post("/profile",bp,function(req,res)
+{
+   
+    
+  
+       usersModel.find({"email":req.body.email},function(err,data)
+        {
+           res.json(data);
+            console.log(data);
+
+       });
+  
+});
+//this routing is used for updating the profile details of the user
+app.post("/profileUpdate",bp,function(req,res)
+{
+    console.log(req.body);
+    usersModel.findOneAndUpdate({"email":req.body.email},{$set:{"accountname":req.body.accountname,
+       "dob":req.body.dob,
+    
+     "phone":req.body.phone,
+    "address.street":req.body.address.street,
+"address.city":req.body.address.city,
+"address.district":req.body.address.district}},{new: true},function(err,data)
+    {
+ console.log(data);
+ res.send({"message":"ok"})
+    });
+  
+});
+
+//this routing is used for inserting the current bookedpackage info into bookedpackages(subdocument)
+//present in user document
+app.post("/booking",bp,function(req,res)
+{
+    
+    var dt=new bookingmodel(req.body.details);
+    console.log(dt);
+    usersModel.findOneAndUpdate({"email":req.body.email},{$push:{"bookedpackages":dt}},function(err,data)
+    {
+        res.send({"message":"ok"})
+     
+    });
+
+});
+//ths function is used for generating array of dates with the help of no. of days 
+//and start date
+var getDateArray = function(start, days) {
+    var arr = new Array();
+    var dt = new Date(start);
+    while (days--!=0) {
+        arr.push(new Date(dt));
+        dt.setDate(dt.getDate() + 1);
+    }
+    return arr;
+}
+//this routing is used for checking whether  the current booking package journey dates are present in any other 
+//already booked packages of that user
+app.post("/checkclash",bp,function(req,res)
+{
+   var userselected=getDateArray(req.body.depaturedate,req.body.duration);
+  console.log(userselected);
+   usersModel.find({"email":req.body.email},function(err,data)
+   {
+       var sending="ok";
+           for(var i=0;i<data.length;i++)
+           {
+               for(var j=0;j<data[i].bookedpackages.length;j++)
+               {
+                   var generated=getDateArray(data[i].bookedpackages[j].depaturedate,data[i].bookedpackages[j].duration);
+                   console.log(generated);
+                  for(var k=0;k<userselected.length;k++)
+                  {
+                      for(var m=0;m<generated.length;m++)
+                      {
+                          //console.log(generated[m]);
+                         // console.log(userselected[k]);
+                          if(generated[m].getDate()==userselected[k].getDate()&&generated[m].getMonth()==userselected[k].getMonth()&&generated[m].getFullYear()==userselected[k].getFullYear())
+                         sending="not ok";
+                          else
+                          continue;
+                      }
+                  }
+               }
+           }
+           res.send({"message":sending});  
+
+});
+});
+
+//this routing is used for querying user booked packages
+app.post("/mybookedpackages",bp,function(req,res)
+{ 
+    console.log("working")
+    usersModel.find({"email":req.body.email},'bookedpackages',function(err,data)
+    {
+
+
+    res.json(data)
+        console.log(data);
+
+   });
+});
+//this routing is used for cancel bookedpackage
+app.post("/cancelbooking",bp,function(req,res)
+{
+    console.log(req.body);
+    usersModel.findOneAndUpdate({"email":req.body.email},{$pull:{"bookedpackages":{"_id":req.body.id}}},function(err,data)
+    {
+        
+      res.send({"message":"ok"})
+    });
 });
--- js/app.js
+++ js/app.js
@@ -1,37 +1,517 @@
-var app=angular.module("myapp",[]);
-app.controller("myctrl",function($scope)
+
+//index page
+var homeapp=angular.module("home",[]);
+homeapp.controller("homeController",function($scope,$window)
 {
-  $scope.packages=[
+  $scope.checksession=function() //checking for the session when the viewall button is clicked
+  { 
+    if($window.sessionStorage.getItem("userEmail"))
     {
-      "name":'BENGALURU',
-      "duration":"3days/2nights",
-      "price":5000,
-      "img":"vidhana.jpg"
-    },
-    {
-      "name":'MYSORE',
-      "duration":"3days/2nights",
-      "price":5000,
-      "img":"mysorepalace.jpg"
-    },
-    {
-      "name":'MADIKERI',
-      "duration":"3days/2nights",
-      "price":5000,
-      "img":"coorg.jpg"
-    },
-    {
-      "name":'MADIKERI',
-      "duration":"3days/2nights",
-      "price":5000,
-      "img":"coorg.jpg"
-    },
-    {
-      "name":'MADIKERI',
-      "duration":"3days/2nights",
-      "price":5000,
-      "img":"coorg.jpg"
+      $window.location.href="packages.html"
     }
-  ]
+    else
+    alert("please login")
+  }
+  $scope.district=function()//if district image is clicked then store the relevant info in local storage
+  {
+    
+  $window.localStorage.setItem('search','district');
+    $window.location.href="packages.html";
+  };
+  $scope.nature=function()//if nature image is clicked then store the relevant info in local storage
+  {
+   
+    $window.localStorage.setItem('search','nature');
+    $window.location.href="packages.html";
+  };
+  $scope.historical=function()//if historical image is clicked then store the relevant info in local storage
+  {
+    
+    $window.localStorage.setItem('search','historical');
+    $window.location.href="packages.html";
+  };
+  $scope.devotional=function()//if devotional image is clicked then store the relevant info in local storage
+  {
+   
+    $window.localStorage.setItem('search','devotional');
+    $window.location.href="packages.html";
+  };
+  $scope.wildlife=function()//if wildlife image is clicked then store the relevant info in local storage
+  {
+   
+    $window.localStorage.setItem('search','wildlife');
+    $window.location.href="packages.html";
+  };
+  $scope.premium=function()//if premium image is clicked then store the relevant info in local storage
+  {
+   
+    $window.localStorage.setItem('search','premium');
+    $window.location.href="packages.html";
+  };
 
 });
+homeapp.controller("navctrl",function($scope,$window)
+{//this controller is used for making dynamic changes in navigation bar by checking session
+  if($window. sessionStorage.getItem("userEmail"))
+  {
+    $scope.usericon=true;//show the usericon
+  }
+  else{
+    $scope.loginRegister=true;//show login|register nav-item
+  }
+  $scope.logout=function() //ending the session
+  {
+    $window.sessionStorage.removeItem("userEmail");
+   $window.location.href="index.html";
+  }
+
+});
+
+//index page
+
+//this for single page application 
+//pages included in SPA are packages.html,load.html,discription.html and booking.html 
+var app=angular.module("myapp",["ngRoute"]);
+app.config(function($routeProvider){
+  $routeProvider.when("/",{
+      templateUrl:"load.html"
+  }).when("/booking:packageId",{
+    controller:"bookingController",
+    templateUrl:"booking.html"
+  }).when("/description:packageId",{
+      controller:"descriptionController",
+      templateUrl:"description.html"
+  });
+  });
+//this controller is present in packages.html
+app.controller("pkgsctrl",function($scope,$http,$window)
+{
+  //checking whether user as stored any info in localstorage ie; whether they are asking any particular type of packages
+  //if any particular type of packages are asking then query the packages collection based on that type
+  if(!$window.localStorage.getItem('search'))
+ {
+  
+  $http.get("http://localhost:3000/packages").then(function(response){
+  
+  $scope.packages=response.data;
+    
+});
+  
+
+ } 
+ else
+ {
+   let data={"searchdata":$window.localStorage.getItem('search')};
+   $scope.searchbox=$window.localStorage.getItem('search');
+   $window.localStorage.removeItem('search');
+   $http.post("http://localhost:3000/fetchBySearch",data).then(function(response)
+   {
+     if(response.data.length!=0)
+     {$scope.packages=response.data
+      $scope.noResults=false;
+     }
+     else
+     {$scope.packages=response.data
+       $scope.noResults=true;
+     }
+
+   });
+ }
+ //this function is used for search system
+ $scope.searching=function()
+ {
+   let data={"searchdata":$scope.searchbox};
+   $http.post("http://localhost:3000/fetchBySearch",data).then(function(response)
+   {
+    if(response.data.length!=0)
+     {$scope.packages=response.data
+      $scope.noResults=false;
+     }
+     else
+     {
+      $scope.packages=response.data
+       $scope.noResults=true;
+     }
+
+
+   });
+ }
+
+});
+
+app.controller("logctrl",function($scope,$window)
+{
+$scope.logout=function()
+{
+ $window.sessionStorage.removeItem("userEmail");
+ $window.location.href="index.html";
+}
+
+  
+});
+//this controller is present in description.html 
+app.controller("descriptionController",function($scope,$http,$routeParams){
+  let data={"packageId":$routeParams.packageId};
+  $http.post("http://localhost:3000/fetchById",data).then(function(response){
+    //formating the date to dd/mm/yyy because it is in UTC format
+  let d=new Date(response.data[0].depaturedate);
+  let enddate=new Date(response.data[0].depaturedate);
+  let visitingplaces="";
+  response.data[0].depaturedate=d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();  
+  for(var i=1;i<=response.data[0].duration;i++)
+  {
+     enddate.setDate(enddate.getDate()+1)
+  }
+  response.data[0].enddate=enddate.getDate()+'/'+(enddate.getMonth()+1)+'/'+enddate.getFullYear();
+  for(var j=0;j<response.data[0].visitingplaces.length;j++)
+   visitingplaces=visitingplaces+","+response.data[0].visitingplaces[j];
+   response.data[0].places=visitingplaces;
+  $scope.details=response.data;
+    console.log("given");
+    
+});
+
+});
+//this controller is present in booking.html
+ app.controller("bookingController",function($scope,$http,$routeParams,$window)
+ {
+  let data={"packageId":$routeParams.packageId};
+  var total;
+  var adultprice;
+  var childprice=0;
+  var duration;
+  var depatureaddress;
+  var depaturedate;
+  var packagename;
+  var icon;
+  //get the info of a booking package and hold those data in above variables
+  $http.post("http://localhost:3000/fetchById",data).then(function(response){
+   var details=response.data;
+  
+ for(var i=0;i<details.length;i++)
+ {
+  $scope.price=details[i].cost;
+  duration=details[i].duration;
+  depatureaddress=details[i].depatureaddress;
+  depaturedate=details[i].depaturedate;
+  packagename=details[i].name;
+  icon=details[i].icon;
+  total=$scope.price;
+  adultprice=total;
+ }
+ });
+ // - button of number of adults
+ $scope.decradult=function()
+ {
+   if(($scope.countad)-1==0)
+   $scope.countad=1;
+   else
+   
+     $scope.countad--;
+   adultprice=total*$scope.countad;
+   $scope.price=adultprice+childprice;
+ 
+}
+// + button of number of adults
+ $scope.incradult=function()
+ {
+  $scope.countad++;
+  adultprice= total*$scope.countad;
+  $scope.price=adultprice+childprice;
+ }
+ // + button of number of children
+ $scope.incrchild=function()
+ {
+  $scope.countcd++;
+  childprice=total*$scope.countcd*0.5;
+  $scope.price=adultprice+childprice;
+ }
+ // - button of number of children
+ $scope.decrchild=function()
+ {
+   if(($scope.countcd)-1<0)
+   $scope.countcd=0;
+   else
+   $scope.countcd--;
+   childprice=total*$scope.countcd*0.5;
+   $scope.price=adultprice+childprice;
+ }
+ //this function is used for checking whether the current booking package journey dates are present in any other
+ //already booked packages of that user 
+ $scope.dateclash=function()
+ {
+   let data={
+     "duration":duration,
+     "depaturedate":depaturedate,
+     "email":$window.sessionStorage.getItem("userEmail")
+   }
+  $http.post("http://localhost:3000/checkclash",data).then(function(response)
+  {
+    if(response.data.message=="not ok")
+    $scope.sorry=true;
+    else
+    $scope.payment=true;
+      $scope.paybutton=true;  
+  });
+  
+ }
+//if their is no dateofclash then this function will be called
+$scope.submitbooking=function()
+{
+  var enddate=new Date(depaturedate);
+  for(var j=1;j<=duration;j++)
+  {
+     enddate.setDate(enddate.getDate()+1)
+  }
+
+  let data={
+                "email":$window.sessionStorage.getItem("userEmail"),
+                "details":{
+                  "packageId":$routeParams.packageId,
+                  "packagename":packagename,
+                  "packageicon":icon,
+                  "enddate":enddate,
+                  "depaturedate":depaturedate,
+                  "depatureaddress":depatureaddress,
+                  "duration":duration,
+                  "adults":$scope.countad,
+                  "children":$scope.countcd,
+                  "dobooking": new Date(),
+                  "totalcost":$scope.price,
+                  "payment":{"cardno":$scope.cardno,"cardname":$scope.cardname,"dateofexpire":$scope.dateofexpire,"cvc":$scope.cvc}
+                }
+  }
+  $http.post("http://localhost:3000/booking",data).then(function(response)
+  {
+        if(response.data.message=="ok")
+        $window.location.href="bookedpackages.html";
+
+  });
+}
+
+
+});
+//this for single page application 
+
+
+
+//login page 
+var loginapp=angular.module("myloginapp",[]);
+loginapp.controller("signinctrl",function($scope,$http,$window,$rootScope)
+{//validate and store the email in sessionStorage
+  $scope.checkuser=function()
+  {
+    let data={
+      "email":$scope.Email,
+      "password":$scope.password
+    } ;
+
+    $http.post("http://localhost:3000/signin",data)
+    .then(function(response){
+        
+     
+        if(response.data.message=="invalidemail")
+        {
+          $rootScope.messageData="Invalid Email";
+          $rootScope.sssiginmessage=true;
+    
+        }
+    else if(response.data.message=="invalidPassword")
+    {
+      
+      $rootScope.messageData="Invalid Password";
+      $rootScope.sssiginmessage=true;
+  
+    }
+         
+    else
+        {
+
+          $window.sessionStorage.setItem("userEmail",response.data.message);
+          $window.location.href="index.html";
+
+        }
+        
+       
+        
+    });
+    
+
+  }
+ 
+    
+});
+
+loginapp.controller("signupctrl",function($scope,$http,$rootScope)
+{//validate and adduser to database
+    $scope.newuser=function()
+    {
+      let data={
+
+        "email":$scope.emailgiven,
+        "password":$scope.password,
+        "accountname":$scope.accountname,
+        "address":{"street":" ", "city":" ", "district":" " }
+      }
+      $http.post("http://localhost:3000/newuser",data)
+      .then(function(response){
+          if(response.data.message=="valid")
+          {
+            $rootScope.sssigup1message=false;
+            $rootScope.invalidEmail=false;
+            $rootScope.sssigupmessage=true;
+            $scope.emailgiven=null;
+            $scope.accountname=null;
+            $scope.password=null;
+          }
+          else if(response.data.message=="InvalidEmail")
+          { $rootScope.sssigup1message=false;
+            $rootScope.sssigupmessage=false;
+           $rootScope.invalidEmail=true
+          }
+          else
+          {
+            $rootScope.sssigupmessage=false;
+            $rootScope.invalidEmail=false;
+            $rootScope.sssigup1message=true;
+          }
+        
+      });
+    }
+});
+
+//login page
+
+
+//profile.html page
+var profileapp=angular.module("profile",[]);
+
+profileapp.controller("profilectrl",function($scope,$http,$window)
+{ //retrieve the basic details of the user and keep track any changes to the input fields
+  let data={"email":$window.sessionStorage.getItem("userEmail")}
+  $http.post("http://localhost:3000/profile",data).then(function(response){
+
+   $scope.accountname=response.data[0].accountname;
+   $scope.phone=response.data[0].phone;
+let d =new Date(response.data[0].dob);
+  $scope.dob=d;
+   $scope.street=response.data[0].address.street;
+   $scope.city=response.data[0].address.city;
+   $scope.district=response.data[0].address.district;
+});
+$scope.savechanges=function()
+{
+  var data ={
+   
+    "accountname":$scope.accountname,
+    "email":$window.sessionStorage.getItem("userEmail"),
+    
+    "phone":$scope.phone,
+    "dob":$scope.dob,
+  
+    "address":{"street":$scope.street, "city":$scope.city, "district":$scope.district }
+  }
+  $http.post("http://localhost:3000/profileUpdate",data).then(function(response)
+  {
+         if(response.data.message=="ok")
+         $window.location.href="profile.html";
+  });
+ 
+} ; 
+
+$scope.resetchanges=function()
+{
+ $window.location.reload();
+};
+
+});
+profileapp.controller("logctrl",function($window,$scope)
+{
+  $scope.logout=function()
+  {
+   $window.sessionStorage.removeItem("userEmail");
+   $window.location.href="index.html";
+  }
+  
+});
+//profile.html page
+
+//bookedpackages.html page
+var bookedapp=angular.module("mybookings",[]);
+
+bookedapp.controller("mybkgctrl",function($scope,$http,$window){
+  var cancelling=[];//this array is used for storing packages which still have cancellation options
+var noncancelling=[];//this array is used for storing packages which doesnot have cancellation options
+let data={"email":$window.sessionStorage.getItem("userEmail")}
+var today=new Date();
+var tomorrow=new Date();
+tomorrow.setDate(tomorrow.getDate()+1);
+  $http.post("http://localhost:3000/mybookedpackages",data).then(function(response)
+  {
+    for(var i=0;i<response.data[0].bookedpackages.length;i++)
+    {
+      var d= new Date(response.data[0].bookedpackages[i].dobooking);
+      d.setDate(d.getDate()+1);
+      if((d.getDate()==today.getDate()||d.getDate()==tomorrow.getDate())&&(d.getMonth()==today.getMonth()||d.getMonth()==tomorrow.getMonth())&&(d.getFullYear()==today.getFullYear()||d.getFullYear()==tomorrow.getFullYear()))
+      {let dobooking=new Date(response.data[0].bookedpackages[i].dobooking);
+        let depaturedate=new Date(response.data[0].bookedpackages[i].depaturedate);
+        let enddate=new Date(response.data[0].bookedpackages[i].enddate)
+        response.data[0].bookedpackages[i].dobooking=dobooking.getDate()+'/'+(dobooking.getMonth()+1)+'/'+dobooking.getFullYear();
+        response.data[0].bookedpackages[i].depaturedate=depaturedate.getDate()+'/'+(depaturedate.getMonth()+1)+'/'+depaturedate.getFullYear();
+        response.data[0].bookedpackages[i].enddate=enddate.getDate()+'/'+(enddate.getMonth()+1)+'/'+enddate.getFullYear();
+        cancelling.push(response.data[0].bookedpackages[i]);
+      }
+      else
+     {
+      let dobooking=new Date(response.data[0].bookedpackages[i].dobooking);
+      let depaturedate=new Date(response.data[0].bookedpackages[i].depaturedate);
+      let enddate=new Date(response.data[0].bookedpackages[i].enddate)
+      response.data[0].bookedpackages[i].dobooking=dobooking.getDate()+'/'+(dobooking.getMonth()+1)+'/'+dobooking.getFullYear();
+      response.data[0].bookedpackages[i].depaturedate=depaturedate.getDate()+'/'+(depaturedate.getMonth()+1)+'/'+depaturedate.getFullYear();
+      response.data[0].bookedpackages[i].enddate=enddate.getDate()+'/'+(enddate.getMonth()+1)+'/'+enddate.getFullYear();
+     
+
+     noncancelling.push(response.data[0].bookedpackages[i])
+     } 
+    }
+$scope.cancelling=cancelling;
+$scope.noncancelling=noncancelling;
+
+  });
+  //if cancel button is clicked 
+  $scope.cancellation=function(id)
+{
+  if (confirm("Are sure want to cancel ")) {
+    let data={
+       'id':id,
+      "email":$window.sessionStorage.getItem("userEmail") }
+       $http.post("http://localhost:3000/cancelbooking",data).then(function(response)
+       {
+            if(response.data.message=="ok")
+            $window.location.href="bookedpackages.html";
+
+       });
+
+  } else {
+    
+  }
+}
+
+});
+bookedapp.controller("logctrl",function($window,$scope)
+{
+  $scope.logout=function()
+{
+ $window.sessionStorage.removeItem("userEmail");
+ $window.location.href="index.html";
+}
+
+});
+//bookedpackages.html page
+
+
+
+
+
+
+
